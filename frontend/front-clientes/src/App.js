a2163fa32fd66b9adf54d711366a1a8c35420ec3
Guilherme Rocha <gleite98@gmail.com>
2021-03-26 12:35:22 -0300

    Exibindo clientes no console.log

--- frontend/front-clientes/src/App.js
+++ frontend/front-clientes/src/App.js
@@ -1,30 +1,117 @@
 import React, {Component} from 'react';
-import $ from 'jquery';
 
 export default class App extends Component {
 
-  onFormSubmitSuccess(e) {
-
-    $.ajax({
-      url: 'http://localhost/newmConexao/listar.php',
-      type: "GET",
-      dataType: "json",
-      success: function(data) {
-        console.log('success');
-    console.log(data); // will print "message"
-  }.bind(this),
-  error: function(xhr, status, err) {
-    console.log('error');
-  }.bind(this)
-});
+  constructor(props){
+    super(props);
+    this.state={
+      customers:[],
+      isLoaded: false,
+      error: '',
+      data: []
+    }
+    this.showCustomers = this.showCustomers.bind(this);
   }
+
+  componentDidMount(){
+
+    fetch('http://localhost/newmConexao/listar.php')
+    .then(res => res.json())
+    .then(
+      (result) => {
+
+        this.setState({
+          isLoaded: true,
+          customers: result
+        });
+      },
+        // error handler
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        },
+        )
+    .then(this.showCustomers)
+  }
+
+  showCustomers(){
+
+    var data = [];
+
+    for (var i=0; i<this.state.customers.length; i++){
+      for(var j=0; j<this.state.customers[i].length; j++){
+
+        //console.log(this.state.customers[i][j]);
+        var cell = this.state.customers[i][j].split(":");
+        data.push(cell[1]);
+
+        
+      }
+    }
+
+    this.setState({
+      data: data
+    });
+
+    for (var i=0; i<this.state.data.length; i++){
+      console.log(this.state.data[i]);
+    }
+  }
+
 
   render(){
+    let state = this.state;
+    /*
+          <h1>Lista de inspeção</h1>
 
-    return (
-      <div className="App">
-        <button onClick={this.onFormSubmitSuccess.bind(this)} >click here</button>
+      <div>
+        {
+          this.state.customers.map((cliente, i) => (
+            <article key={cliente.id}>
+              <strong>{cliente.nome}</strong>
+              <p>dt_inicio: {cliente.nascimento}</p>
+              <p>dt_fim: {cliente.telefone}</p>
+              <p>Nome: {this.state.cliente[i].nome}</p>
+            </article>
+          ))
+        }
       </div>
-      );
+      */
+      for (var i=0; i<this.state.data.length; i++){
+        return (<div>{state.data[i]}</div>);
+      }
+
+      return (
+        <div className="App">
+
+
+
+
+        {state.customers.length}
+        {state.data[1]}
+
+
+        </div>
+        );
+    }
   }
-}
+
+  class Customer extends Component{
+
+    constructor(props){
+      super(props);
+    }
+
+    render(){
+
+      console.log(this.props.customers[0]);
+
+      return(
+        <>
+        <p>{this.props.customers}</p>
+        </>
+        )
+      }
+    }
